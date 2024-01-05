119275c82fe73fd41007b130318714f20147fa35
pui-yi <153875169+pui-yi@users.noreply.github.com>
2024-01-04 17:58:23 -0800

    changed the route path for home from /home to /; the code and link are now cleaner.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,6 @@ import { HomeComponent } from './home/home.component';
 // const routes: Routes = [];
 const routes: Routes = [
   { path: '', component: HomeComponent }, 
-  { path: 'home', component: HomeComponent }, 
   { path: 'products', component: ProductsComponent }, 
   { path: 'contact', component: ContactComponent }, 
 ];
